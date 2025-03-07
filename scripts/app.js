9927c0e6c1571a914ffb7ee8546c7d1a024819ba
matheus_lima <matheus_lima@gpssa.com.br>
2025-03-07 11:22:34 -0300

    Alterando a agenda dos eventos

--- scripts/app.js
+++ scripts/app.js
@@ -27,7 +27,16 @@ const eventsCollection = collection(db, "events");
 
 getDocs(eventsCollection).then((snapshot) =>{
     snapshot.forEach((doc) => {
-        //console.log(doc.id, " => ", doc.data());
+        console.log(doc.id, " => ", doc.data());
+        //console.log(doc.id, " => ", doc.data().date);
+        //console.log(doc.id, " => ", doc.data().tags);
+        //let tagText = ""
+        //for (let x of (doc.id, " => ", doc.data().tags)) {
+        //    tagText += x + " + "
+        //}
+        //tagText = tagText.slice(0, -2)
+        //console.log(tagText)
+        console.log(doc.id, " => ", doc.data().url)
         renderEvent(doc);
     });
 });
@@ -36,18 +45,47 @@ const eventList = document.querySelector("#agenda-list");
 
 function renderEvent(doc){
     
+    //cria os elementos em html
     let div = document.createElement('div');
+    let span = document.createElement('span');
     let name = document.createElement('h5');
+    let date_tag = document.createElement('p')
+    let time = document.createElement('p')
     let description = document.createElement('p');
     let local = document.createElement('p');
-
+    let img = document.createElement('img');
+    
+    
+    //pega os dados do banco
     name.textContent = doc.data().name;
+    
+    let tagText = ""
+    for (let x of (doc.data().tags)){
+        tagText += x + " + "
+    }
+    tagText = tagText.slice(0, -2)
+    
+    let date = doc.data().date;
+    date_tag.innerHTML = `<p><strong>${date}</strong> • ${tagText}</p>`;
     description.textContent = doc.data().description;
+    
+    let startTime = doc.data().time_start;
+    let endTime = doc.data().time_end;
+
+    time.textContent = `${startTime} ás ${endTime}`
+
     local.textContent = doc.data().local;
 
-    div.appendChild(name);
-    div.appendChild(description);
-    div.appendChild(local);
+    img.src = doc.data().url; 
+    
+    //coloca os texteo no html
+    span.appendChild(name);
+    span.appendChild(date_tag);
+    (doc.data().description.trim() && span.appendChild(description));
+    (startTime.trim() || endTime.trim()) && span.appendChild(time);
+    span.appendChild(local);
+    div.appendChild(span);
+    div.appendChild(img)
     
     eventList.appendChild(div);
 }
--- scripts/scroll-nav.js
+++ scripts/scroll-nav.js
@@ -1,19 +1,19 @@
-document.querySelectorAll('nav a').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
+document.querySelectorAll("nav a").forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
 
-        const targetId = this.getAttribute('href');
-        const targetElement = document.querySelector(targetId);
-        const nav = document.getElementById('nav');
-        const shadow = document.getElementById('nav-shadow')
+    const targetId = this.getAttribute("href");
+    const targetElement = document.querySelector(targetId);
+    const nav = document.getElementById("nav");
+    const shadow = document.getElementById("nav-shadow");
 
-        window.scrollTo({
-            top: targetElement.offsetTop,
-            behavior: 'smooth'
-        });
-
-        nav.classList.remove('nav-open');
-        shadow.style.visibility = 'hidden';
-        shadow.style.opacity = '0';
+    window.scrollTo({
+      top: targetElement.offsetTop,
+      behavior: "smooth",
     });
+
+    nav.classList.remove("nav-open");
+    shadow.style.visibility = "hidden";
+    shadow.style.opacity = "0";
+  });
 });
